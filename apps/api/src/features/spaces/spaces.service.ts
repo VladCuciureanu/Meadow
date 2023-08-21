518233b8971c17e739a446fe578aa4288f383db4
Vlad Cuciureanu <vlad.cuciureanu@pm.me>
2023-08-21 22:30:36 +0300

    fix: Space update

--- apps/api/src/features/spaces/spaces.service.ts
+++ apps/api/src/features/spaces/spaces.service.ts
@@ -1,4 +1,4 @@
-import { Repository } from "typeorm";
+import { Repository, DeepPartial } from "typeorm";
 import { Space } from "./space.model";
 import { MeadowDataSource } from "../../config/typeorm";
 import { z } from "zod";
@@ -18,24 +18,54 @@ class SpacesService {
 
   async getMany(limit: number, page: number) {
     const skipCount = Math.max(0, limit * (page - 1));
-    return this.spacesRepository.find({ take: limit, skip: skipCount });
+    return this.spacesRepository.find({
+      take: limit,
+      skip: skipCount,
+      relations: ["team"],
+    });
   }
 
   async getById(spaceId: string) {
-    return this.spacesRepository.findOne({ where: { id: spaceId } });
+    return this.spacesRepository.findOne({
+      where: { id: spaceId },
+      relations: ["team"],
+    });
   }
 
   async create(dto: z.infer<typeof CreateSpaceSchema>) {
-    const space = this.spacesRepository.create(dto.body);
+    const payload: DeepPartial<Space> = {
+      ...dto.body,
+    };
+
+    payload.team = { id: dto.body.teamId };
+
+    const space = this.spacesRepository.create(payload);
     return this.spacesRepository.save(space);
   }
 
   async patch(dto: z.infer<typeof PatchSpaceSchema>) {
-    return this.spacesRepository.update({ id: dto.params.spaceId }, dto.body);
+    const payload: DeepPartial<Space> = {
+      ...dto.body,
+    };
+
+    if (payload.teamId) {
+      payload.team = { id: payload.teamId };
+      delete payload.teamId;
+    }
+
+    return this.spacesRepository.update({ id: dto.params.spaceId }, payload);
   }
 
   async put(dto: z.infer<typeof UpdateSpaceSchema>) {
-    return this.spacesRepository.update({ id: dto.params.spaceId }, dto.body);
+    const payload: DeepPartial<Space> = {
+      ...dto.body,
+    };
+
+    if (payload.teamId) {
+      payload.team = { id: payload.teamId };
+    }
+
+    return this.spacesRepository.update({ id: dto.params.spaceId }, payload);
   }
 
   async delete(dto: z.infer<typeof DeleteSpaceSchema>) {
